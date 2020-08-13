a5525d6fea0bbdfb66ff588e284fdfee88a1c17f
r3k4t <69615463+r3k4t@users.noreply.github.com>
2020-08-13 09:21:21 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+Linux
+
+This python program can get all wifi user ip and mac address.At first you can install nmap and python-nmap in your linux.
+
+Windows
+
+At first you can install nmap stable software and python-nmap module.
+
+Linux Example :
+
+1.cd WifiUser
+2.sudo python wifiuser.py
+ 
+![Screenshot at 2020-08-13 08-08-59](https://user-images.githubusercontent.com/69572771/90089021-fddbb780-dd3d-11ea-88b2-742ba6d25bf9.png)
---
+++ wifiuser.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import nmap
+os.system("clear")
+os.system("figlet -f standard Wifi  User")
+print
+print 'Author : Rahat Khan Tusar(RKT)'
+print
+print 'Information : This program can get all wifi user ip and mac address.'
+print
+ip = raw_input("Router IP :")
+nm = nmap.PortScanner()
+nm.scan('127.0.0.1', '22-443')
+nm.command_line()
+nm.scaninfo()
+nm.all_hosts()
+nm.all_hosts()
+nm['127.0.0.1'].hostname()
+nm['127.0.0.1'].state()
+nm['127.0.0.1'].all_protocols()
+nm['127.0.0.1']['tcp'].keys()
+nm['127.0.0.1'].all_tcp()
+print (nm.scan(hosts='192.168.0.0/24', arguments='-n -sP -PE -PA21,23,80,3389'))
